88c77c1ae072c847e4f2d3198ce0af65cc119bb7
Isaac Luo <yishaluo@gmail.com>
2019-03-28 15:09:43 +0000

    able to generate genbank file

--- src/components/Assemble.tsx
+++ src/components/Assemble.tsx
@@ -3,6 +3,8 @@ import {Dropdown, Button, Table} from 'react-bootstrap';
 import styled from 'styled-components';
 
 import CONNECTORS from '../connectors.json'
+import {IFeature, DNASeq} from '../gbGenerator';
+import vectorReceiver from '../vectorReceiver.json';
 
 const Panel = styled.div`
   margin:100px;
@@ -26,23 +28,56 @@ interface IProps {
   onClickNext?: (selectedParts:any)=>void,
 }
 interface IState {
+  finalParts: any[],
+  genbank: string,
 }
 
 export default class Assemble extends React.Component<IProps, IState> {
   constructor (props:IProps) {
-    super(props);
-    
+    super(props); 
   }
-  public componentWillReceiveProps(newProps: IProps) {
-    
+  public componentWillMount() {
+    this.mergeParts();
   }
-  public componentDidMount() {
-    
-  }
-  public componentDidUpdate() {
-    
+  public componentWillUpdate() {
+    this.mergeParts();
   }
   public render() {
+    const {finalParts} = this.state;
+
+    return <Panel>
+      <Table striped={true} bordered={true} hover={true}>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>sequence</th>
+          </tr>
+        </thead>
+        <tbody>
+          {finalParts.map((v,i)=>
+              <tr key={i}>
+                <td>{v.name}</td>
+                <td>
+                  <RedSpan>
+                    {v.sequence.substr(0,4)}
+                  </RedSpan>
+                  {v.sequence.substring(4, v.sequence.length-4)}
+                  <RedSpan>
+                    {v.sequence.substr(v.sequence.length-4,4)}
+                  </RedSpan>
+                  </td>
+              </tr>)}
+          <tr>
+          </tr>
+        </tbody>
+      </Table>
+      <div>
+        <Button variant="primary" size="lg" onClick={this.downloadGenbank}>download genbank</Button>
+      </div>
+    </Panel>
+  }
+
+  private mergeParts () {
     const shortCuts = this.props.selectedShortcuts.map(v=>CONNECTORS[v.idx]);
     const {selectedParts} = this.props;
 
@@ -50,6 +85,7 @@ export default class Assemble extends React.Component<IProps, IState> {
     let i=0;
     let j=0;
     const re = [];
+
     while(i<shortCuts.length && j<selectedParts.length) {
       const idxI = shortCuts[i].pos[0];
       const idxJ = selectedParts[j].ctype.idx;
@@ -71,33 +107,46 @@ export default class Assemble extends React.Component<IProps, IState> {
       j++;
     }
 
+    this.setState({finalParts: re, genbank: this.generateGenbank(re)});
+    
+  }
 
-    return <Panel>
-      <Table striped={true} bordered={true} hover={true}>
-        <thead>
-          <tr>
-            <th>name</th>
-            <th>sequence</th>
-          </tr>
-        </thead>
-        <tbody>
-          {re.map((v,i)=>
-              <tr key={i}>
-                <td>{v.name}</td>
-                <td>
-                  <RedSpan>
-                    {v.sequence.substr(0,4)}
-                  </RedSpan>
-                  {v.sequence.substring(4, v.sequence.length-4)}
-                  <RedSpan>
-                    {v.sequence.substr(v.sequence.length-4,4)}
-                  </RedSpan>
-                  </td>
-              </tr>)}
-          <tr>
-          </tr>
-        </tbody>
-      </Table>
-    </Panel>
+  private generateGenbank (parts:any[]) {
+    const dnaSeq = new DNASeq({
+      sequence: vectorReceiver.seqeunce,
+      features: vectorReceiver.features,
+    });
+
+    parts.forEach(part => {
+      
+      const sequence = part.sequence.substr(0,part.sequence.length-4);
+      const begin = dnaSeq.sequence.length;
+
+      dnaSeq.sequence += sequence;
+      dnaSeq.features.push({
+        from: begin,
+        to: begin + sequence.length,
+        strand: '.',
+        ctype: 'misc_feature',
+        label: part.name,
+      });
+
+    })
+
+    return dnaSeq.toGenbank();
+  }
+
+  private downloadGenbank = () => {
+    const {genbank} = this.state;
+    const a = window.document.createElement('a');
+    a.href = window.URL.createObjectURL(new Blob([genbank], {type: 'text/plain'}));
+    a.download = 'result.gb';
+
+    // Append anchor to body.
+    document.body.appendChild(a);
+    a.click();
+
+    // Remove anchor from body
+    document.body.removeChild(a);
   }
 }
