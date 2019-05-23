238a605cd5a9bc0339dd2c0c333c80b6cc1193b0
Isaac Luo <yishaluo@gmail.com>
2019-05-23 12:28:22 +0100

    use production env

--- frontend/src/conf.ts
+++ frontend/src/conf.ts
@@ -1,4 +1,10 @@
-export default {
-  serverURL: 'http://local.cailab.org:8000',
+const conf = {
+  serverURL: 'https://api.lims.cailab.org',
   authServerURL: 'https://api.auth.cailab.org',
 };
+
+if (process.env.NODE_ENV === 'development') {
+  conf.serverURL = 'http://local.cailab.org:8000'
+}
+
+export default conf;
