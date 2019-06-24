0129c2093d126ce031b06edc0a44de4639a676ad
Isaac Luo <yishaluo@gmail.com>
2019-06-24 18:18:19 +0100

    more ProtocolView.tsx

--- frontend/src/components/ProtocolView.tsx
+++ frontend/src/components/ProtocolView.tsx
@@ -65,6 +65,8 @@ class ProtocolView extends React.Component<IProps, IState> {
   }
   public render() {
 
+    const sampleCount = this.props.project.parts.filter(v=>v.selected).length + this.props.project.connectorIndexes.length;
+
     return <React.Fragment>
       <Breadcrumb>
         <Breadcrumb.Item href='/projects'>Home</Breadcrumb.Item>
@@ -80,6 +82,7 @@ class ProtocolView extends React.Component<IProps, IState> {
       <Title2>
         Equipment/Reagents:
       </Title2>
+      <ul>
       <Li>Thermocycler</Li>
       <Li>T4 DNA ligase 400U/µL (NEB: M0202S)</Li>
       <Li>10X T4 ligase reaction buffer (included in NEB: M0202S)</Li>
@@ -89,9 +92,63 @@ class ProtocolView extends React.Component<IProps, IState> {
       <Li>ATP solution, 25 mM (included in Epicenter: E3110K)</Li>
       <Li>Part plasmids (50 ng/µL)</Li>
       <Li>Receiver plasmid (10 ng/µL)</Li>
-
+      </ul>
+      <ol>
+        <Li>On ice, prepare a Golden Gate assembly MM. For each reaction, mix together
+            1 µL 10X T4 DNA Ligase Reaction Buffer, 0.1 µL BSA, 0.5 µL FastDigest Esp3I
+            and 0.25 µL T4 DNA ligase 400 U/µL. Multiply the volume for each component
+            by the number of reactions. Mix the final solution gently.
+        </Li>
+        <Table bordered hover>
+          <tr>
+            <th></th>
+            <th>volume per component</th>
+            <th>volume for {sampleCount} items</th>
+          </tr>
+          <tr>
+            <th>Reagent</th>
+            <td>10μL reaction</td>
+            <td>{sampleCount*10}μL reaction</td>
+          </tr>
+          <tr>
+            <th>T4 Ligase Reaction Buffer</th>
+            <td>1µL</td>
+            <td>{sampleCount*1}μL</td>
+          </tr>
+          <tr>
+            <th>BSA</th>
+            <td>0.1µL</td>
+            <td>{(sampleCount*0.1).toFixed(2)}μL</td>
+          </tr>
+          <tr>
+            <th>Fast Digest Esp3I</th>
+            <td>0.5µL</td>
+            <td>{(sampleCount*0.5).toFixed(2)}μL</td>
+          </tr>
+          <tr>
+            <th>T4 DNA Ligase</th>
+            <td>0.25µL</td>
+            <td>{(sampleCount*0.25).toFixed(2)}μL</td>
+          </tr>
+        </Table>
+        <Li>
+          Next, place on ice as many 0.2 mL PCR tubes as assemblies 
+          to be carried out. To each tube, add an equimolar amount of 
+          each part plasmid (13 fmol)
+          and 0.5 µL of the receiver vector (10 ng/µL). 
+          Add nuclease-free water to a final volume of 8.15 µL . 
+        </Li>
+      </ol>
     </Panel>
     </React.Fragment>
+  }
+
+  private calcDNAMass(mol:number, dnaLen:number) {
+    return mol * dnaLen * 617.96 + 36.04;
+  }
+
+  private calcDNAVolume(mass:number) {
+    return mass / 50;
   }
 }
 
